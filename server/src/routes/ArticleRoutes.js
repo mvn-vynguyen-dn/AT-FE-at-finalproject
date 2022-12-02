d3dfd51f2130a9f9c9bb1fc146360c41c704713e
Vy Nguyen V <vy.nguyen@monstar-lab.com>
2022-12-02 11:31:25 +0700

    test

--- server/src/routes/ArticleRoutes.js
+++ server/src/routes/ArticleRoutes.js
@@ -4,7 +4,7 @@ var router = express.Router();
 var article = require("../controllers/ArticleControllers");
 
 router.post("/asd", article.create);
-router.get("/", article.index);
+router.get("/asd", article.index);
 router.get("/:id", article.show);
 router.put("/:id", article.update);
 router.delete("/:id", article.delete);
